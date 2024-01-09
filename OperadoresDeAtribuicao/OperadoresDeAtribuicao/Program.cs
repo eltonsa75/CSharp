bb85c6f37a15ab78605222c931ee8aa2b4229678
Elton Andrade <eltonsa75@hotmail.com>
2024-01-09 13:03:11 -0300

    Oeradores de Atribuição

---
+++ OperadoresDeAtribuicao/OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OperadoresDeAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+            s += "DEF";
+            Console.WriteLine(s);
+
+            a++;
+            Console.WriteLine(a);
+
+            a--;
+            Console.WriteLine(a);
+
+            int b = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
