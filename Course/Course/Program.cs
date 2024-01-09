b0eb035901d36649ed79059587193a01db9332f2
Elton Andrade <eltonsa75@hotmail.com>
2024-01-09 12:12:17 -0300

    Saida de dados no C#

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,9 +7,32 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            sbyte x = 100;
 
-            Console.WriteLine(x);
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+
+            /* Diferença de Writeline(faz quebra de linha no final) e Write */
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa Tarde!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            /* Colocando ponto de separador */
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais", nome, idade, saldo);
+
+
+
         }
     }
 }
