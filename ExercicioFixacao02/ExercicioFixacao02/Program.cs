d26d53c20b75ac4cac92c51dcc264051c5500cd8
Elton Andrade <eltonsa75@hotmail.com>
2024-01-10 15:13:40 -0300

    Exercicio de fixação 02

---
+++ ExercicioFixacao02/ExercicioFixacao02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace exerciciofixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem sua casa:");
+            string quartos = Console.ReadLine();
+            Console.WriteLine("Entre com o preço de um produto:");
+            string produtos = Console.ReadLine();
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)");      
+         
+            string[] vet = Console.ReadLine().Split(' ');
+
+            string nomel = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            
+
+            Console.WriteLine(nomel);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+        }
+    }
+}
