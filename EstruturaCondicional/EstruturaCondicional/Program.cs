7646a73b4a16c0aa8232c30ff2a8405c6fb7de4e
Elton Andrade <eltonsa75@hotmail.com>
2024-01-18 10:55:35 -0300

    Estrutura simples, Composta, Encadeamento

--- EstruturaCondicional/EstruturaCondicional/Program.cs
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -4,16 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre com um número inteiro:");
-            int x = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
 
-            if(x % 2 == 0) {
-                Console.WriteLine("Par!");
-            } 
+            if (hora < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa Tarde!");
+            }
             else
             {
-                Console.WriteLine("Impar!");
-            }
+                Console.WriteLine("Boa Noite!");
+            } 
         }
     }
 }
