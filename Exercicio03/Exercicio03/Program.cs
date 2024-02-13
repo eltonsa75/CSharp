74150ccebf0642e4ac2622748bd52b7a08570862
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-13 13:35:36 -0300

    Exercicio 03 Altura e Largura do Retângulo

---
+++ Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Retangulo Ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            Ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + Ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + Ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + Ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
