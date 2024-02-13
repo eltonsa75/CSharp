5f5ce7eef057b5e29465e2962db95a23fd63dde0
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-13 11:39:19 -0300

    Criando o estoque usando ToString()

--- CriandoAreaDoTriangulo/CriandoAreaDoTriangulo/Triangulo.cs
+++ CriandoAreaDoTriangulo/CriandoAreaDoTriangulo/Triangulo.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using system;
+
+
 namespace CriandoAreaDoTriangulo
 {
     class Triangulo
@@ -6,5 +8,13 @@ namespace CriandoAreaDoTriangulo
         public double A;
         public double B;
         public double C;
+
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
     }
 }
---
+++ Estoque/Estoque/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+
+            Console.WriteLine("Entre os dados do Produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto " + p);
+
+
+        }
+    }
+}
+
