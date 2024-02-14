35879cd1864882bd340b020f170da53d0c717051
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-14 18:00:58 -0300

    Properties - Aula - 56

--- Constructor/Constructor/Produto.cs
+++ Constructor/Constructor/Produto.cs
@@ -7,39 +7,65 @@ namespace Constructor
     internal class Produto
     {
 
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto(string nome, double preco, int quantidade)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 0;
         }
+
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if(nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+           
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
 
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProduto(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return Nome 
+            return _nome 
                 + ", $ " 
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + "," 
-                + Quantidade
+                + _quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)
                 ;
--- Constructor/Constructor/Program.cs
+++ Constructor/Constructor/Program.cs
@@ -8,35 +8,17 @@ namespace Contructors
     {
         static void Main(string[] args)
         {
-            
 
-            Console.WriteLine("Entre os dados do produto:");
-            Console.Write("Nome: ");
-             string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade no estoque: ");
-            int quantidade = int.Parse(Console.ReadLine());
+            Produto p = new Produto("TV", 500.00, 10);
 
-            Produto p = new Produto(nome, preco, quantidade);
+            p.SetNome("TV 4k");
 
-            Console.WriteLine();
-            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco);
+            Console.WriteLine(p.GetQuantidade);
 
-            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
 
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
 
-            Console.WriteLine();
-            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProduto(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
 
         }
     }
