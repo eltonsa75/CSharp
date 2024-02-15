eec847e3b149fc69916f16135c1b0333326ce00a
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-15 11:20:32 -0300

    Exercicio Conta Bancaria

--- Constructor/Constructor/Produto.cs
+++ Constructor/Constructor/Produto.cs
@@ -8,16 +8,17 @@ namespace Constructor
     {
 
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco {  get; private set; }
+        public int Quantidade { get; private set; }
+     
 
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = 0;
+            Preco = preco;
+            Quantidade = quantidade;
+          
         }
-
 
         public string Nome
         {
@@ -25,45 +26,33 @@ namespace Constructor
             set { 
             
             if ( value != null && value.Length > 1) {
-                    _nome = value;
+                    Nome = value;
                 }
             }
         }
 
-        public double Preco
-        {
-            get { return _preco; }
-        }
-
-        public int Quantidade
-        {
-            get { return _quantidade; }
-        }
-
-
-
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
 
         public void RemoverProduto(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return _nome 
+            return _nome
                 + ", $ " 
-                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
                 + "," 
-                + _quantidade
+                + Quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)
                 ;
---
+++ Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("dados da conta: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
