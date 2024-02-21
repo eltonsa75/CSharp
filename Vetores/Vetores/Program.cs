65bf2f8fa0c0d357500b4af8aee3e36d010233c0
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-21 10:42:54 -0300

    Vetores

--- Vetores/Vetores/Program.cs
+++ Vetores/Vetores/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Vetores 
 {
@@ -9,6 +10,22 @@ namespace Vetores
             int n = int.Parse(Console.ReadLine());
 
             Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                vect[i] = new Product { Nome = nome, Price = price };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n;i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("AVAREGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
 
 
         }
