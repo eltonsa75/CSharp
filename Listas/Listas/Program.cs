14a6cddb7cf162f6487ec6ba7a77d2c676325655
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-21 16:48:15 -0300

    Criando uma list com Insert

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("ELton");
+            list.Add("Amanda");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
