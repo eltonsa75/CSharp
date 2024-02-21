30a4069f5f230008ca478e7107c62e277850d5bf
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-21 10:35:06 -0300

    Criando Vetores

---
+++ Structs/Structs/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Structs
+{
+    struct Point
+    {
+        public double X; 
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+   
+}
---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vetores 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+
+        }
+    }
+
+}
