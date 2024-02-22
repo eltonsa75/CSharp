6c5d12d3c34a7e8d41ec62bf4035177a4335d2c6
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-02-22 09:38:28 -0300

    Listas com Foreach

--- Listas/Listas/Program.cs
+++ Listas/Listas/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Threading.Channels;
 
 namespace Listas
 {
@@ -19,6 +20,31 @@ namespace Listas
             {
                 Console.WriteLine(obj);
             }
+            Console.WriteLine("List count: " + list.Count);
+
+            //Predicado 
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+
+            int post1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First Position 'A': " + post1);
+
+            int post2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last Position 'A': " + post2);
+           
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("=============================");
+            foreach (string obj in list2) {
+                Console.WriteLine(obj);
+            }
         }
+
+        
+
     }
 }
