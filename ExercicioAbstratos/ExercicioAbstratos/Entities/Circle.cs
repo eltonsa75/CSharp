7c023a90c8e602b8a899e66d88312fadb0dd731b
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-03-30 13:27:21 -0300

    Exercicio usando o métodos Abstratos.

---
+++ ExercicioAbstratos/ExercicioAbstratos/Entities/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ExercicioAbstratos.Entities.Enums;
+ 
+
+namespace ExercicioAbstratos.Entities
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ ExercicioAbstratos/ExercicioAbstratos/Entities/Shape.cs
@@ -0,0 +1,17 @@
+﻿
+using ExercicioAbstratos.Entities.Enums;
+
+namespace ExercicioAbstratos.Entities
+{
+    abstract  class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
