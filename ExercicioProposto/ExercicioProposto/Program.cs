2eb0b5a7d43644231bb064722d28ed515a4363c8
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-03-31 17:30:33 -0300

    Exercicio proposto realizado com sucesso.

---
+++ ExercicioProposto/ExercicioProposto/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using ExercicioProposto.Etities;
+using ExercicioProposto.Etities.Exceptions;
+
+namespace ExercicioProposto;
+
+public class ExercicioProposto
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter account data");
+        Console.Write("Number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("Holder: ");
+        String holder = Console.ReadLine();
+        Console.Write("Initial balance: ");
+        double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Withdraw limit: ");
+        double withdrawLimit = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        Console.WriteLine();
+        Console.Write("Enter amount for withdraw: ");
+        double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        try
+        {
+            acc.Withdraw(amount);
+            Console.WriteLine("Nem Balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        catch (DomainException e)
+        {
+            Console.WriteLine("Withdraw error: " + e.Message);
+        }
+    }
+}
