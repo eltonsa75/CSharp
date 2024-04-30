c6430122109ecddb9ee2b31aa6947358a41b3d68
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-04-29 21:37:13 -0300

    Trabalhando com File, FileInfo

---
+++ File/File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\User\Documents\file1.txt";
+            string targetPath = @"C:\Users\User\Documents\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
