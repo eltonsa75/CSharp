e99fb34dd8b28c715a3b31e03f4b7abb4431feea
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-02 10:46:38 -0300

    Criando Interface de Aluguel de Carros

---
+++ AluguelCarros/AluguelCarros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+using AluguelCarros.Entities;
+
+
+namespace AluguelCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:ss): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:ss): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
