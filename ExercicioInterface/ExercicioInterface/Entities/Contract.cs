4549c96de06d0410b2d58f212aae3d449ea5ab60
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-03 19:40:23 -0300

    Exercicio que parcela contrato usando Interface

---
+++ ExercicioInterface/ExercicioInterface/Entities/Contract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ExercicioInterface.Entities
+{
+    internal class Contract
+    {
+        public int number {  get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> Installments { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            this.number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installments = new List<Installment>();
+        }
+
+        public void AddInstallment(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}
---
+++ ExercicioInterface/ExercicioInterface/Entities/Installment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioInterface.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            this.Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+            +" - "
+            + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
