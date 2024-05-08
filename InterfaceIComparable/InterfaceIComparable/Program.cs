1b87bf313e19b05fbecf426cb5542fd147291bd3
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-08 10:37:49 -0300

    Ordenando a lista por nome ou salary

---
+++ InterfaceIComparable/InterfaceIComparable/Program.cs
@@ -0,0 +1,38 @@
+﻿using InterfaceIComparable.Entities;
+using System;
+using InterfaceIComparable.Entities;
+
+namespace InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\CSharp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    // Ordenar a lista
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
