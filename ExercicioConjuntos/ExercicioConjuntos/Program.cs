e78950d708b088e0f6c0fa3e896fa03e19bba4b9
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-08 17:11:39 -0300

    Exercicio de conjuntos resolvidos

---
+++ ExercicioConjuntos/ExercicioConjuntos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ExercicioConjuntos.Entities;
+
+namespace ExercicioConjuntos
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.WriteLine("enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                    Console.WriteLine("total users: " + set.Count);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
