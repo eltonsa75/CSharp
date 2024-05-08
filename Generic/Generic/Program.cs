8e7b59807bd94441c78be68b092a5c1e63ab3600
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-08 11:27:50 -0300

    Generic finalizado

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -6,6 +6,19 @@ namespace Generic
     {
         static void Main(string[] args)
         {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.addValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
 
         }
     }
