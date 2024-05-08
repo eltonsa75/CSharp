cd16991da43e45ea7c51c46c9cf2f52fe294fe58
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-08 15:16:44 -0300

    HashSet parte01

---
+++ HashSorted/HashSorted/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Table");
+
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
