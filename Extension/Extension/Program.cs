bd4e69062d379d28ba43f34b510fcac1b3f4429a
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-09 09:52:47 -0300

    Extensions String

--- Extension/Extension/Program.cs
+++ Extension/Extension/Program.cs
@@ -1,10 +1,15 @@
-﻿using Extension.Extensions;
+﻿
+using Extension.Extensions;
 using System;
 public class ProcessFile
 {
     public static void Main()
     {
-        DateTime dt = new DateTime(2024, 05, 01, 8, 10, 45);
-        Console.WriteLine(dt.ElapsedTime());
+        //DateTime dt = new DateTime(2024, 05, 01, 8, 10, 45);
+        //Console.WriteLine(dt.ElapsedTime());
+
+        string s1 = "Good morning dear students!";
+        Console.WriteLine(s1.Cut(10));
+
     }
 }
