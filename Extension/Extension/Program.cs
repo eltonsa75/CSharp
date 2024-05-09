70a3a0d358ab549eaf8fea64f209329fb77376c0
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-09 09:45:08 -0300

    Extensions de Datetime

---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,10 @@
+﻿using Extension.Extensions;
+using System;
+public class ProcessFile
+{
+    public static void Main()
+    {
+        DateTime dt = new DateTime(2024, 05, 01, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
+    }
+}
