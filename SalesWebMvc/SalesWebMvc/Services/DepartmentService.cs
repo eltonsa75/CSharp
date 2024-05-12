9312f30b24f97d5c21a5e0d29b86a3b736299bd0
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-12 19:06:39 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/SalesWebMvc/Services/DepartmentService.cs
@@ -1,8 +1,11 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModel;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace SalesWebMvc.Services
 {
@@ -16,9 +19,9 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
