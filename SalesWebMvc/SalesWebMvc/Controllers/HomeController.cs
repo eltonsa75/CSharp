247bd4da1dfd3345f7e28df85b2717e120a8ad07
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-12 22:06:53 -0300

    Implementing grouping search

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models.ViewModel;
-using SalesWebMvc.Models.ViewModel;
+
 
 namespace SalesWebMvc.Controllers
 {
